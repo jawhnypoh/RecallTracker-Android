b9dbed057863bb2723d28f6624fb6928c9c9cd62
shieldse <ethan.shields@cdk.com>
2019-08-20 12:54:19 -0700

    Added code to retrieve push token

--- app/src/main/java/com/example/recalltracker/MainActivity.java
+++ app/src/main/java/com/example/recalltracker/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.recalltracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = Objects.requireNonNull(task.getResult()).getToken();
+                        Log.d(TAG, "Token: " + token);
+
+                        // Log and toast
+                        Log.d(TAG, "Retrieved firebase push token");
+                        Toast.makeText(MainActivity.this, "Retrieved firebase push token", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
