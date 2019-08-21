0e373ab2e05e9aa2135f39ee96b70d136c492808
downerc <carson.downer@cdk.com>
2019-08-21 07:37:28 -0700

    Added Cardview to ResultsActivity and setResults()

--- app/src/main/java/com/example/recalltracker/ResultsActivity.java
+++ app/src/main/java/com/example/recalltracker/ResultsActivity.java
@@ -9,22 +9,33 @@ import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
 
-    TextView MFG_Campaign_number, notes;
-
     Typeface roboto;
 
+    String Campaign_Number, Note, NoteDescription, CampaignDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        Campaign_Number = "Egg Salad";
+        CampaignDescription = "Yummy yummy";
+        Note = "Baby Shark";
+        NoteDescription = "doo doo doo";
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_results);
+
         TextView MFG_Campaign_number = findViewById(R.id.MFG_Campaign_Number);
         TextView notes = findViewById(R.id.Notes);
-        MFG_Campaign_number.setText("Hello");
-        notes.setText("Existence is pain");
+        setResults(Campaign_Number,CampaignDescription,MFG_Campaign_number);
+        setResults(Note,NoteDescription,notes);
+        //setResults(Campaign_Number,CampaignDescription,MFG_Campaign_number);
     }
+
+    protected void setResults(String descriptor, String description, TextView textview) {
+        textview.setText(descriptor + ": " + description);
+
+    }
+
 
 
 }
