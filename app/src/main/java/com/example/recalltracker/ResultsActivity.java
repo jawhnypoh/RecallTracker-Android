877130782ea92f1458124259d23e61c073fc7b29
downerc <carson.downer@cdk.com>
2019-08-21 09:38:39 -0700

    Added Defect and Consequence Textviews

--- app/src/main/java/com/example/recalltracker/ResultsActivity.java
+++ app/src/main/java/com/example/recalltracker/ResultsActivity.java
@@ -11,7 +11,7 @@ public class ResultsActivity extends AppCompatActivity {
 
     Typeface roboto;
 
-    String Campaign_Number, Note, NoteDescription, CampaignDescription;
+    String Campaign_Number, Note, NoteDescription, CampaignDescription, Defect, Defectresults, Consequence, Consequences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +20,23 @@ public class ResultsActivity extends AppCompatActivity {
         CampaignDescription = "Yummy yummy";
         Note = "Baby Shark";
         NoteDescription = "doo doo doo";
+        Defect = "What not to break";
+        Defectresults = "My heart";
+        Consequence = "Heartache";
+        Consequences = "From broken heart";
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_results);
 
         TextView MFG_Campaign_number = findViewById(R.id.MFG_Campaign_Number);
         TextView notes = findViewById(R.id.Notes);
+        TextView defect = findViewById(R.id.Defect);
+        TextView consequence = findViewById(R.id.Consequences);
         setResults(Campaign_Number,CampaignDescription,MFG_Campaign_number);
         setResults(Note,NoteDescription,notes);
+        setResults(Defect,Defectresults,defect);
+        setResults(Consequence,Consequences,consequence);
         //setResults(Campaign_Number,CampaignDescription,MFG_Campaign_number);
     }
 
