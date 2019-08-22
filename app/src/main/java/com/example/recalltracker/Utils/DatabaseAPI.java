b03224f193a6664f6fed858329611990d6c947df
shieldse <ethan.shields@cdk.com>
2019-08-22 13:31:41 -0700

    Refactored Database API

--- app/src/main/java/com/example/recalltracker/Utils/DatabaseAPI.java
+++ app/src/main/java/com/example/recalltracker/Utils/DatabaseAPI.java
@@ -5,6 +5,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.ArrayList;
@@ -13,24 +14,33 @@ import java.util.Map;
 
 public class DatabaseAPI {
 
-    public static void getUser(CollectionReference users, String uid, OnCompleteListener<DocumentSnapshot> onCompleteListener) {
+    private String userId;
+    private CollectionReference users;
+
+    public DatabaseAPI(String userId) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        this.users = db.collection("users");
+        this.userId = userId;
+    }
+
+    public void getUser(CollectionReference users, String uid, OnCompleteListener<DocumentSnapshot> onCompleteListener) {
         DocumentReference docRef = users.document(uid);
         docRef.get().addOnCompleteListener(onCompleteListener);
     }
 
-    public static void updateUser(final CollectionReference users, final String uid, final Map<String, Object> data) {
-        DocumentReference docRef = users.document(uid);
+    public void updateUser(final Map<String, Object> data) {
+        DocumentReference docRef = users.document(userId);
         data.put("updated", System.currentTimeMillis());
         docRef.set(data, SetOptions.merge());
     }
 
-    public static void addVehicle(final CollectionReference users, final String uid, final VehicleItem vehicle) {
+    public void addVehicle(final VehicleItem vehicle) {
         Map<String, Object> data = new HashMap<>();
-        ArrayList<VehicleItem> vehicles = new ArrayList<VehicleItem>();
+        ArrayList<VehicleItem> vehicles = new ArrayList<>();
         vehicles.add(vehicle);
         data.put("vehicles", vehicles);
 
-        updateUser(users, uid, data);
+        updateUser(data);
     }
 
     public static Map<String, Object> createUserData(String pushToken, boolean notificationsEnabled, ArrayList<VehicleItem> vehicles) {
