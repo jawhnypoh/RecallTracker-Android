66434a254620a03fa46c65309e6223fd7d4b305d
shieldse <ethan.shields@cdk.com>
2019-08-22 15:46:06 -0700

    Added imports

--- app/src/main/java/com/example/recalltracker/VehiclesListActivity.java
+++ app/src/main/java/com/example/recalltracker/VehiclesListActivity.java
@@ -7,9 +7,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.recalltracker.Models.VehicleItem;
 import com.example.recalltracker.Utils.DatabaseAPI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
